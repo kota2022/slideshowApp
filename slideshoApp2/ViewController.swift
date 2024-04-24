6366bfab5a8911381d23c968810c72d2e65b0673
kota tsuji <kota.tsuji2022@klas.ch>
2024-04-24 11:28:22 +0900

    Initial Commit

---
+++ slideshoApp2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  slideshoApp2
+//
+//  Created by Tsuji Kota on 24.04.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
